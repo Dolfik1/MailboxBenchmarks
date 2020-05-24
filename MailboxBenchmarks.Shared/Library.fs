1679874e0ecf658d321d94efc0ac38cd4c7cced4
Nikolay Matyushin <dolphinn@bk.ru>
2020-05-24 17:06:18 +0300

    Hopac and channel

---
+++ MailboxBenchmarks.Shared/Library.fs
@@ -0,0 +1,11 @@
+﻿namespace MailboxBenchmarks.Shared
+
+open System.Threading.Tasks
+
+type Message =
+  | Unit
+  | Done of TaskCompletionSource<unit>
+  member x.TryDone(tcs: TaskCompletionSource<unit> byref) =
+      match x with
+      | Done(t) -> tcs <- t; true
+      | _ -> false
